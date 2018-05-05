b4d7b032b6e02dce4e1903e81f43e950e1e7d40a
Hiroki (REO) Kashiwazaki <reo@samar.local>
2018-05-05 17:55:07 +0900

    change indents

--- init.el
+++ init.el
@@ -22,58 +22,58 @@
 (setq ns-alternate-modifier (quote super))
 
 
-(if window-system (progn
-
-		    ;; 文字の色を設定します。
-		    (add-to-list 'default-frame-alist '(foreground-color . "gray10"))
+(if window-system
+    (progn
+      ;; 文字の色を設定します。
+      (add-to-list 'default-frame-alist '(foreground-color . "gray10"))
 		    ;; 背景色を設定します。
-		    (add-to-list 'default-frame-alist '(background-color . "white"))
-		    ;; カーソルの色を設定します。
-		    (add-to-list 'default-frame-alist '(cursor-color . "SlateBlue2"))
-		    ;; マウスポインタの色を設定します。
-		    (add-to-list 'default-frame-alist '(mouse-color . "SlateBlue2"))
-		    ;; モードラインの文字の色を設定します。
-		    (set-face-foreground 'mode-line "white")
-		    ;; モードラインの背景色を設定します。
-		    (set-face-background 'mode-line "MediumPurple2")
-		    ;; 選択中のリージョンの色を設定します。
-		    (set-face-background 'region "LightSteelBlue1")
-		    ;; モードライン（アクティブでないバッファ）の文字色を設定します。
-		    (set-face-foreground 'mode-line-inactive "gray30")
-		    ;; モードライン（アクティブでないバッファ）の背景色を設定します。
-		    (set-face-background 'mode-line-inactive "gray85")
+      (add-to-list 'default-frame-alist '(background-color . "white"))
+      ;; カーソルの色を設定します。
+      (add-to-list 'default-frame-alist '(cursor-color . "SlateBlue2"))
+      ;; マウスポインタの色を設定します。
+      (add-to-list 'default-frame-alist '(mouse-color . "SlateBlue2"))
+      ;; モードラインの文字の色を設定します。
+      (set-face-foreground 'mode-line "white")
+      ;; モードラインの背景色を設定します。
+      (set-face-background 'mode-line "MediumPurple2")
+      ;; 選択中のリージョンの色を設定します。
+      (set-face-background 'region "LightSteelBlue1")
+      ;; モードライン（アクティブでないバッファ）の文字色を設定します。
+      (set-face-foreground 'mode-line-inactive "gray30")
+      ;; モードライン（アクティブでないバッファ）の背景色を設定します。
+      (set-face-background 'mode-line-inactive "gray85")
 
-		    ;; for source code
-		    (set-face-foreground 'font-lock-comment-face "MediumSeaGreen")
-		    (set-face-foreground 'font-lock-string-face  "purple")
-		    (set-face-foreground 'font-lock-keyword-face "blue")
-		    (set-face-foreground 'font-lock-function-name-face "blue")
-		    (set-face-bold-p 'font-lock-function-name-face t)
-		    (set-face-foreground 'font-lock-variable-name-face "black")
-		    (set-face-foreground 'font-lock-type-face "LightSeaGreen")
-		    (set-face-foreground 'font-lock-builtin-face "purple")
-		    (set-face-foreground 'font-lock-constant-face "black")
-		    (set-face-foreground 'font-lock-warning-face "blue")
-		    (set-face-bold-p 'font-lock-warning-face nil)
+      ;; for source code
+      (set-face-foreground 'font-lock-comment-face "MediumSeaGreen")
+      (set-face-foreground 'font-lock-string-face  "purple")
+      (set-face-foreground 'font-lock-keyword-face "blue")
+      (set-face-foreground 'font-lock-function-name-face "blue")
+      (set-face-bold-p 'font-lock-function-name-face t)
+      (set-face-foreground 'font-lock-variable-name-face "black")
+      (set-face-foreground 'font-lock-type-face "LightSeaGreen")
+      (set-face-foreground 'font-lock-builtin-face "purple")
+      (set-face-foreground 'font-lock-constant-face "black")
+      (set-face-foreground 'font-lock-warning-face "blue")
+      (set-face-bold-p 'font-lock-warning-face nil)
 
-		    ;; ウィンドウ位置・大きさの指定
-		    (add-to-list 'initial-frame-alist '(width . 80))
-		    (add-to-list 'initial-frame-alist '(top . 0))
-		    (if (string-match "visayas" system-name)
-			(setq initial-frame-alist '((height . 61)(left . 1120))))
-		    (when (string-match "agalega" system-name)
-		      (add-to-list 'default-frame-alist '(font . "ricty-16"))
-		      (setq initial-frame-alist '((height . 68)(left . 1220))) nil)
-		    (when (string-match "samar" system-name)
-		      (add-to-list 'default-frame-alist '(font . "ricty-16"))
-		      (setq initial-frame-alist '((height . 84)(left . 1880))) nil)
-		    (when (string-match "balleny" system-name)
-		      (add-to-list 'default-frame-alist '(font . "ricty-14"))
-		      (setq initial-frame-alist '((height . 53)(left . 840))) nil)
-		    (when (string-match "chatham" system-name)
-		      (add-to-list 'default-frame-alist '(font . "ricty-16"))
-		      (setq initial-frame-alist '((height . 60)(left . 1000))) nil)
-		    ))
+      ;; ウィンドウ位置・大きさの指定
+      (add-to-list 'initial-frame-alist '(width . 80))
+      (add-to-list 'initial-frame-alist '(top . 0))
+      (if (string-match "visayas" system-name)
+	  (setq initial-frame-alist '((height . 61)(left . 1120))))
+      (when (string-match "agalega" system-name)
+	(add-to-list 'default-frame-alist '(font . "ricty-16"))
+	(setq initial-frame-alist '((height . 68)(left . 1220))) nil)
+      (when (string-match "samar" system-name)
+	(add-to-list 'default-frame-alist '(font . "ricty-16"))
+	(setq initial-frame-alist '((height . 84)(left . 1880))) nil)
+      (when (string-match "balleny" system-name)
+	(add-to-list 'default-frame-alist '(font . "ricty-14"))
+	(setq initial-frame-alist '((height . 53)(left . 840))) nil)
+      (when (string-match "chatham" system-name)
+	(add-to-list 'default-frame-alist '(font . "ricty-16"))
+	(setq initial-frame-alist '((height . 60)(left . 1000))) nil)
+      ))
 
 (set-language-environment "Japanese")
 (set-clipboard-coding-system 'sjis-mac)
